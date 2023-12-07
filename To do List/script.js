a82efdad9bdd7a8d80a6aced90c856a57fe7d5c1
imsouravhassan <iamsouravhassan@gimai.com>
2023-12-08 00:26:14 +0600

    To do List

---
+++ To do List/script.js
@@ -0,0 +1,38 @@
+
+// Html Element Select
+
+let userInput = document.querySelector('.user-input');
+let addBtn = document.querySelector('.add-btn');
+let main = document.querySelector('.main');
+
+ // User Input Array Call
+const inputData = [];
+
+// Add button functionality
+
+function add(){
+  let userValue = userInput.value;
+  inputData.push(userValue);
+  userInput.value ="";
+
+  displayItem();
+}
+
+
+// display itme
+
+
+function displayItem(){
+    let newHtml = '<div class = "input-div">'
+    inputData.forEach((value,index)=>{
+        if(value !==""){
+            newHtml += `<div><span class="item">${value}</span>
+            <button style="delete"  onclick = "inputData.splice(${index},1); displayItem();">Delete</button></div>`
+        }
+       })
+     newHtml +='</div>'
+      main.innerHTML = newHtml;
+
+}
+
+addBtn.addEventListener('click',add)
